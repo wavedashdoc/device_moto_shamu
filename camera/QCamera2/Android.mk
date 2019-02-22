9a7ae8c61a054beaed45dd2ced83cecf85b2013a
elektroschmock <elektroschmock78@googlemail.com>
2019-02-22 13:19:49 +0100

    shamu: camera: Compile with -Wall and -Werror
    Change-Id: Iff63d797e92dbfd836eec87a2337cd7ab66720ce
    (cherry picked from commit 84aa22ff0e3bd9aa1d2a5e8414b6eb9143ac1d85)

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -5,18 +5,11 @@ include $(CLEAR_VARS)
 # QCameraParameters.h has unused private field.
 # QCamera2Hal.cpp, QCamera3HWI.cpp, etc. use GNU old-style field designator extension.
 # QCamera3PostProc.cpp has unused label.
-# QCamera3HWI.cpp, QCamera3PostProc.cpp etc. have unused variable.
 # QCamera3Channel.cpp compares array to null pointer.
-# QCamera2Factory.cpp, QCamera3HWI.cpp, etc. have unused parameter.
-# QCamera3HWI.cpp has print format error.
 LOCAL_CLANG_CFLAGS += \
-        -Wno-error=unused-private-field \
-        -Wno-error=gnu-designator \
-        -Wno-error=unused-label \
-        -Wno-error=unused-variable \
-        -Wno-error=unused-parameter \
-        -Wno-error=tautological-pointer-compare \
-        -Wno-error=format
+    -Wno-error=unused-private-field \
+    -Wno-error=unused-label \
+    -Wno-error=tautological-pointer-compare
 
 LOCAL_SRC_FILES := \
     util/QCameraCmdThread.cpp \
