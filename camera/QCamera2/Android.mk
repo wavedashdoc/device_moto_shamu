e788eeaa5ac8967d8dda1fd424340bf1efb51cb7
elektroschmock <elektroschmock78@googlemail.com>
2019-02-22 10:00:52 +0100

    shamu: camera: Cleanup makefiles
    Change-Id: I2c2d6009e7239b637fd95dd9cae4f76acebec97e
    (cherry picked from commit acde2119f9004250747f85e497caf2b72c8eaf22)

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -1,5 +1,3 @@
-ifneq (,$(filter $(TARGET_ARCH), arm arm64))
-
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -21,38 +19,44 @@ LOCAL_CLANG_CFLAGS += \
         -Wno-error=format
 
 LOCAL_SRC_FILES := \
-        util/QCameraCmdThread.cpp \
-        util/QCameraQueue.cpp \
-        util/QCameraFlash.cpp \
-        QCamera2Hal.cpp \
-        QCamera2Factory.cpp
+    util/QCameraCmdThread.cpp \
+    util/QCameraQueue.cpp \
+    util/QCameraFlash.cpp \
+    QCamera2Hal.cpp \
+    QCamera2Factory.cpp
 
 #HAL 3.0 source
 LOCAL_SRC_FILES += \
-        HAL3/QCamera3HWI.cpp \
-        HAL3/QCamera3Mem.cpp \
-        HAL3/QCamera3Stream.cpp \
-        HAL3/QCamera3Channel.cpp \
-        HAL3/QCamera3VendorTags.cpp \
-        HAL3/QCamera3PostProc.cpp
+    HAL3/QCamera3HWI.cpp \
+    HAL3/QCamera3Mem.cpp \
+    HAL3/QCamera3Stream.cpp \
+    HAL3/QCamera3Channel.cpp \
+    HAL3/QCamera3VendorTags.cpp \
+    HAL3/QCamera3PostProc.cpp
 
 #HAL 1.0 source
 LOCAL_SRC_FILES += \
-        HAL/QCamera2HWI.cpp \
-        HAL/QCameraMem.cpp \
-        HAL/QCameraStateMachine.cpp \
-        HAL/QCameraChannel.cpp \
-        HAL/QCameraStream.cpp \
-        HAL/QCameraPostProc.cpp \
-        HAL/QCamera2HWICallbacks.cpp \
-        HAL/QCameraParameters.cpp \
-        HAL/QCameraThermalAdapter.cpp
+    HAL/QCamera2HWI.cpp \
+    HAL/QCameraMem.cpp \
+    HAL/QCameraStateMachine.cpp \
+    HAL/QCameraChannel.cpp \
+    HAL/QCameraStream.cpp \
+    HAL/QCameraPostProc.cpp \
+    HAL/QCamera2HWICallbacks.cpp \
+    HAL/QCameraParameters.cpp \
+    HAL/QCameraThermalAdapter.cpp
 
-LOCAL_CFLAGS := -Wall -Werror
-LOCAL_CFLAGS += -DHAS_MULTIMEDIA_HINTS
+LOCAL_CFLAGS := \
+    -DHAS_MULTIMEDIA_HINTS \
+    -Wall \
+    -Werror
+
 
 #HAL 1.0 Flags
-LOCAL_CFLAGS += -DDEFAULT_DENOISE_MODE_ON -DHAL3 -DVANILLA_HAL
+LOCAL_CFLAGS += \
+    -DDEFAULT_DENOISE_MODE_ON \
+    -DHAL3 \
+    -DVANILLA_HAL
 
 LOCAL_C_INCLUDES := \
         $(LOCAL_PATH)/stack/common \
@@ -66,8 +70,8 @@ LOCAL_C_INCLUDES := \
 
 #HAL 1.0 Include paths
 LOCAL_C_INCLUDES += \
-        frameworks/native/include/media/hardware \
-        device/moto/shamu/camera/QCamera2/HAL
+    frameworks/native/include/media/hardware \
+    device/moto/shamu/camera/QCamera2/HAL
 
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
@@ -92,5 +96,3 @@ include $(BUILD_SHARED_LIBRARY)
 include $(LOCAL_PATH)/HAL/test/Android.mk
 
 include $(call first-makefiles-under,$(LOCAL_PATH))
-
-endif
