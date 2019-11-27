0daff5a90d589807d477159fae6baeca475b04cd
wavedashdoc <kbix18@gmail.com>
2019-11-27 10:54:54 -0500

    Revert: shamu: camera: even more fixes to align with Lineage 15.1
    AOSiP Doesn't support call "project-path-for". Thanks to Elektroschmock for discovering.

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -51,7 +51,7 @@ LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/stack/common \
     frameworks/native/include/media/hardware \
     frameworks/native/include/media/openmax \
-    $(call project-path-for,qcom-media)/libstagefrighthw \
+    hardware/qcom/media/msm8974/libstagefrighthw \
     system/media/camera/include \
     $(LOCAL_PATH)/../mm-image-codec/qexif \
     $(LOCAL_PATH)/../mm-image-codec/qomx_core \
@@ -67,8 +67,9 @@ LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 #LOCAL_STATIC_LIBRARIES := libqcamera2_util
 LOCAL_C_INCLUDES += \
-    $(call project-path-for,qcom-display)/libgralloc \
-    $(call project-path-for,qcom-display)/libqdutils
+        hardware/qcom/display/$(TARGET_BOARD_PLATFORM)/libgralloc
+LOCAL_C_INCLUDES += \
+        hardware/qcom/display/$(TARGET_BOARD_PLATFORM)/libqdutils
 
 LOCAL_SHARED_LIBRARIES := \
     libcamera_client \
